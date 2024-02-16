04b50356fccb8e07fd9c3e49b0c204a92422ff45
Mehdi Merkachi <merkachimehdi@gmail.com>
2024-02-16 23:36:12 +0100

    feat: Install Breeze and Tailwind CSS
    This commit installs Breeze, a starter kit for Laravel authentication and user management, and integrates Tailwind CSS into the project.
    
    Changes Made:
    -  Added the Breeze package using Composer.
    -  Ran the necessary migration and published the Breeze assets.
    -  Configured the authentication routes and views.
    -  Integrated Tailwind CSS by installing the package via npm or Yarn.
    -  Set up the required configuration files for Tailwind CSS.
    -  Customized the project styles using Tailwind CSS utility classes.
    
    These additions provide a streamlined authentication system with Breeze and enhance the project's UI using Tailwind CSS.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        return view('home');
+//        get 3 first posts
+        $posts = Post::all()->take(3);
+
+        return view('home', compact('posts'));
     }
 }
