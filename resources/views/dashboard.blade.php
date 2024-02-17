2358585933daafb97eef483e168802f53f2c921c
Mehdi Merkachi <merkachimehdi@gmail.com>
2024-02-17 18:32:59 +0100

    feat: Display posts from the database in dashboard.blade.php by passing them through DashboardController.php.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,17 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6">
+                @foreach($posts as $post)
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 dark:text-gray-100 space-y-6">
+                            <x-post-card :datetime="$post->created_at->format('M d, Y')" :imageSrc="$post->image"
+                                         :title="$post->title" :content="$post->content"/>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
+
 </x-app-layout>
